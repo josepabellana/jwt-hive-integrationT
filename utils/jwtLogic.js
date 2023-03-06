eea57146c989ba505fcbdd0df7830323de2aa5b8
Josep Abellana <josep.abe.pu@gmail.com>
2023-03-06 09:39:43 +0100

    x

--- utils/jwtLogic.js
+++ utils/jwtLogic.js
@@ -5,17 +5,18 @@ const {
 } = require("@hivestreaming/hive-jwt-auth");
 
 async function computePublicKey(data) {
+    //CREATE PRIVATE KEY
   const file = "path/private-key.pem"; // File to save PEM-encoded private key
   const keyPair = await HiveKeyPair.create();
   await keyPair.writePrivateKey(file);
 
+  //CREATE JWT
   const partnerId = "9001"; // Partner Id
   const keyId = "key-id"; // Key Id
-  const customerId = "customer-id"; // Customer Id
+  const customerId = "15"; // Customer Id
   const videoId = "video-id"; // Event/Video Id
   const manifests = [
-    "https://example.com/manifest.m3u8",
-    "https://www.example.com/manifest.mpd",
+    'https://streaming-simulator-prod.hivestreaming.com/generic/live/beta-big-bunny-multi/manifest.m3u8'
   ]; // Manifests
   const expiresIn = "15 minutes"; // Expires in. See documentation of `HiveJwtCreator#sign` for format details.
   const eventName = "event test name"; // Event name
@@ -31,8 +32,9 @@ async function computePublicKey(data) {
 
   console.log(jwt);
 
+  //PUBLISH PUBLIC KEY
   const partnerToken = "foobar"; // Partner Token
-  const endpoint = "prod"; // Endpoint: 'test' or 'prod'. Default if none provided: 'prod'
+  const endpoint = "test"; // Endpoint: 'test' or 'prod'. Default if none provided: 'prod'
   const expiration = 30; // Expiration. See documentation of `HivePublicKeyServiceClient#create` for format details.
 
   const publicKey = keyPair.exportPublicKey();
@@ -43,6 +45,8 @@ async function computePublicKey(data) {
     endpoint
   );
 
+  console.log(client)
+
   await client.create({
     partnerId,
     expiration,
@@ -50,25 +54,7 @@ async function computePublicKey(data) {
     ...publicKey,
   });
 
-  // const hivePluginOptions = {
-  //   debugLevel: "debug",
-  //   hiveTechOrder: ["HiveJava", "HiveJS", "StatsJS"],
-  //   playerName: "",
-  //   playerVersion: "",
-  // };
-  const plugin = new HiveHtml5(player, options);
-
-  plugin
-    .initSession({
-      jwt: jwt,
-      videoId: videoId,
-      manifest: manifests[0],
-    })
-    .then((session) => {
-      enableHiveXhr(plugin, player);
-      // load player source according to player API
-      player.load(session.manifest);
-    });
+  
 }
 
 
--- validation.js
+++ validation.js
@@ -1,13 +1,30 @@
-
-
 (function () {
-  amp.plugin("jwt", function (options) {
-    
+  amp.plugin("jwt", async function (options) {
     let player = this;
     var init = function () {
       console.log("plugin jwt initialized with player ", player);
     };
-    fetch('http://localhost:3001/jwtHive');
+    fetch("http://localhost:3001/jwtHive");
+
+    const hivePluginOptions = {
+        debugLevel: "debug",
+        hiveTechOrder: ["HiveJava", "HiveJS", "StatsJS"],
+        playerName: "microsoft",
+        playerVersion: "",
+      };
+    const plugin = new HiveHtml5(player, hivePluginOptions);
+
+    plugin
+      .initSession({
+        jwt: jwt,
+        videoId: videoId,
+        manifest: manifests[0],
+      })
+      .then((session) => {
+        enableHiveXhr(plugin, player);
+        // load player source according to player API
+        player.load(session.manifest);
+      });
 
     init();
   });
