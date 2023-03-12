8eef4eaf45839ff12328c628a80bf20bbf43453d
Josep Abellana <josep.abe.pu@gmail.com>
2023-03-12 20:37:26 +0100

    x

--- testAxios.js
+++ testAxios.js
@@ -14,8 +14,5 @@ let client = axios.create({
 
 
 // let data = client.get(`/publickey/9001?includeDeleted=false`).then(data => console.log(data)); //list all keys
-    
 let data2 = client.get(`/publickey/9001/key-616`).then(data2=>console.log(data2)).catch(err=>console.log('error'));
-
-
 //this get should return null in https://github.com/hivestreaming/hive-jwt-auth/blob/master/nodejs/src/lib/HivePublicKeyServiceClient.ts
